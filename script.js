34748317c56217e4232adf24098d41545fff13e7
Sarthak Vyas <sarthak7vyas@gmail.com>
2022-06-12 15:42:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -25,13 +25,13 @@ locationBtn.addEventListener("click", () =>{
 });
 
 function requestApi(city){
-    api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=your_api_key`;
+    api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=9a2a283b2f3f087c3ed16d6743cc1736`;
     fetchData();
 }
 
 function onSuccess(position){
     const {latitude, longitude} = position.coords; // getting lat and lon of the user device from coords obj
-    api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=your_api_key`;
+    api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=9a2a283b2f3f087c3ed16d6743cc1736`;
     fetchData();
 }
 
